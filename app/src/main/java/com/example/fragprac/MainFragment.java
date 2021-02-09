1e1b89f4af964cec2631866d448633c05653fed0
hanuuuuU <han980004@naver.com>
2021-02-09 17:02:39 +0900

    fragment1 commit

---
+++ app/src/main/java/com/example/fragprac/MainFragment.java
@@ -0,0 +1,46 @@
+package com.example.fragprac;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class MainFragment extends Fragment {
+    MainActivity activity;
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+
+        activity = (MainActivity) getActivity();
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+
+        activity = null;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_main,container,false);
+
+        //rootView가 최상위 레이아웃이기 때문에 findView할 때 아래와 같이 적는다
+        Button button =(Button) rootView.findViewById(R.id.move1);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                activity.onFragmentChange(1);
+            }
+        });
+
+        return rootView;
+    }
+}
